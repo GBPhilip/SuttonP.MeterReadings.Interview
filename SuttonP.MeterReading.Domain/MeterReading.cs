30a58b538e5f79ccdde7a0914e7cc69777630b05
GBPhilip <10743454+GBPhilip@users.noreply.github.com>
2021-12-11 11:47:23 +0000

    Add meter reading domain class

---
+++ SuttonP.MeterReading.Domain/MeterReading.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SuttonP.MeterReading.Domain
+{
+    internal class MeterReading
+    {
+        public int AccountId { get; set; }
+        public DateTime Taken { get; set; }
+        public string Value { get; set; }
+    }
+}
