37c9220c652ac5c134cbe4df3e2b5ab74611ac6b
GBPhilip <10743454+GBPhilip@users.noreply.github.com>
2021-12-11 11:08:34 +0000

    Add project files.

---
+++ SuttonP.MeterReadings.Data/Class1.cs
@@ -0,0 +1,42 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+
+using Microsoft.EntityFrameworkCore;
+
+using SuttonP.MeterReading.Domain;
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace SuttonP.MeterReadings.Data
+{
+    public class MeterReadingsContext : DbContext
+    {
+        public MeterReadingsContext(DbContextOptions<MeterReadingsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Account> Accounts => Set<Account>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var seedAccounts = GetSeedAccounts();
+            modelBuilder.Entity<Account>().Property(p => p.Id).ValueGeneratedNever();
+            modelBuilder.Entity<Account>().HasData(seedAccounts);
+
+        }
+
+        private IEnumerable<Account> GetSeedAccounts()
+        {
+            using StreamReader inputReader = File.OpenText(@"C:\Users\GBPhi\source\repos\ensek\SuttonP.MeterReadings.Interview\DataFiles\Test_Accounts.csv");
+            CsvConfiguration csvConfiguration = new (CultureInfo.InvariantCulture);
+            using CsvReader csvReader = new(inputReader, csvConfiguration);
+            var accounts = csvReader.GetRecords<Account>();
+            return accounts;
+
+        }
+    }
+}
